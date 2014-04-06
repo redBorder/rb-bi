a612a86dfe774618c271163fca26474986b39b39
Andrés Gomez <andresgomez92@gmail.com>
2014-04-06 11:55:06 +0200

    mseToFlowDelay -> use kafka again (no delay) and prepare other topology without delay

--- src/main/java/com/redborder/storm/trident/topologies/TridentRedBorderTopologies.java
+++ src/main/java/com/redborder/storm/trident/topologies/TridentRedBorderTopologies.java
@@ -14,6 +14,8 @@ import com.redborder.storm.trident.filter.SleepFilter;
 import com.redborder.storm.trident.function.EventBuilderFunction;
 import com.redborder.storm.trident.function.GetFieldFunction;
 import com.redborder.storm.trident.function.GetMSEdata;
+import com.redborder.storm.trident.function.MapToJSONFunction;
+import com.redborder.storm.trident.function.ProducerKafkaFunction;
 import com.redborder.storm.trident.spout.TrindetKafkaSpout;
 import com.redborder.storm.trident.spout.TwitterStreamTridentSpout;
 import com.redborder.storm.trident.state.MemcachedMultipleState;
@@ -30,11 +32,12 @@ import com.redborder.storm.util.state.ConcatKeyBuilder;
 import java.io.FileNotFoundException;
 import java.net.InetSocketAddress;
 import java.util.Arrays;
+import java.util.Properties;
 import storm.trident.Stream;
 import storm.trident.TridentState;
 import storm.trident.TridentTopology;
-import storm.trident.state.StateFactory;
 import storm.trident.operation.builtin.FilterNull;
+import storm.trident.state.StateFactory;
 
 /**
  *
@@ -101,7 +104,7 @@ public class TridentRedBorderTopologies {
         return topology;
     }
 
-    public TridentTopology flowMSEtopology() throws FileNotFoundException {
+    public TridentTopology flowMSEtopologyDelay() throws FileNotFoundException {
         TridentTopology topology = new TridentTopology();
         GetKafkaConfig zkConfig = new GetKafkaConfig();
 
@@ -137,10 +140,22 @@ public class TridentRedBorderTopologies {
         zkConfig.setTopicInt(RBEventType.FLOW);
         StateFactory druidStateFlow = new TridentBeamStateFactory<>(new MyBeamFactoryMapFlow(zkConfig));
 
-        Stream tranquilityStream = flowsMse
+        Properties props = new Properties();
+
+        props.put("metadata.broker.list", "localhost:9092");
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        props.put("request.required.acks", "1");
+
+        flowsMse
                 .project(new Fields("flowMseKO"))
                 .each(new Fields("flowMseKO"), new MSEenrichedFilter("KO"))
-                .each(new Fields("flowMseKO"), new SleepFilter(30000l))
+                .each(new Fields("flowMseKO"), new MapToJSONFunction(), new Fields("jsonFlowMseKO"))
+                .project(new Fields("jsonFlowMseKO"))
+                .each(new Fields("jsonFlowMseKO"), new ProducerKafkaFunction(props, "rb_delay"), new Fields("sendOK"));
+
+        Stream tranquilityStream = topology.newStream("rb_delay", new TrindetKafkaSpout().builder(
+                "localhost:2181", "rb_delay", "kafkaStorm"))
+                .each(new Fields("str"), new EventBuilderFunction(7), new Fields("topic", "flowMseKO"))
                 .each(new Fields("flowMseKO"), new GetFieldFunction("mac_src"), new Fields("mac_src_flow_KO"))
                 .stateQuery(mseState, new Fields("mac_src_flow_KO", "flowMseKO"), new mseQuery("Segundo", "mac_src_flow_KO", "flowMseKO"), new Fields("flowsTranquilityOK", "flowsTranquilityKO"));
 
@@ -151,7 +166,7 @@ public class TridentRedBorderTopologies {
         //.partitionPersist(druidStateFlow, new Fields("flowsTranquility"), new TridentBeamStateUpdater());
 
         tranquilityStream
-        .project(new Fields("flowsTranquilityKO"))
+                .project(new Fields("flowsTranquilityKO"))
                 .each(new Fields("flowsTranquilityKO"), new FilterNull())
                 .each(new Fields("flowsTranquilityKO"), new CorrelationTridentTopology.PrinterBolt("----"), new Fields("b"));
         //.partitionPersist(druidStateFlow, new Fields("flowsTranquility"), new TridentBeamStateUpdater());
@@ -162,6 +177,33 @@ public class TridentRedBorderTopologies {
                 .each(new Fields("flowMseOK"), new CorrelationTridentTopology.PrinterBolt("----"), new Fields("a"));;
 
         //.partitionPersist(druidStateFlow, new Fields("flowMseOK"), new TridentBeamStateUpdater());
+        return topology;
+    }
+
+    public TridentTopology flowMSEtopology() throws FileNotFoundException {
+        TridentTopology topology = new TridentTopology();
+
+        GetKafkaConfig zkConfig = new GetKafkaConfig();
+
+        int PORT = 52030;
+
+        MemcachedMultipleState.Options mseOpts = new MemcachedMultipleState.Options();
+        mseOpts.expiration = 20000;
+        mseOpts.keyBuilder = new ConcatKeyBuilder("MSE");
+        StateFactory memcached = MemcachedMultipleState.transactional(Arrays.asList(new InetSocketAddress("localhost", PORT)), mseOpts);
+
+        zkConfig.setTopicInt(RBEventType.MSE);
+        TridentState mseState = topology.newStream("rb_mse", new TrindetKafkaSpout().builder(
+                zkConfig.getZkConnect(), zkConfig.getTopic(), "kafkaStorm"))
+                .each(new Fields("str"), new EventBuilderFunction(RBEventType.MSE), new Fields("topic", "mseMap"))
+                .project(new Fields("mseMap"))
+                .each(new Fields("mseMap"), new GetMSEdata(), new Fields("mac_src_mse", "geoLocationMSE"))
+                .project(new Fields("mac_src_mse", "geoLocationMSE"))
+                .partitionBy(new Fields("mac_src_mse"))
+                .partitionPersist(memcached, new Fields("geoLocationMSE", "mac_src_mse"), new mseUpdater());
+
+        zkConfig.setTopicInt(RBEventType.FLOW);
+
         return topology;
     }
 
