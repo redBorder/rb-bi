95bad57ea6bcaf2e3f7c08669b080c2d828fdf1f
Andrés Gomez <andresgomez92@gmail.com>
2014-05-27 11:42:54 +0200

    Changed some fields on finalEvent to send to Druid.

--- src/main/java/net/redborder/storm/function/GetMSEdata.java
+++ src/main/java/net/redborder/storm/function/GetMSEdata.java
@@ -50,21 +50,21 @@ public class GetMSEdata extends BaseFunction {
             longitude = (double) Math.round(longitude * 100000) / 100000;
             locationFormat = lattitude.toString() + "," + longitude.toString();
 
-            mseData.put("sta_mac_address_lat", lattitude.toString());
-            mseData.put("sta_mac_address_long", longitude.toString());
-            mseData.put("sta_mac_address_latlong", locationFormat);
+            mseData.put("client_lat", lattitude.toString());
+            mseData.put("client_long", longitude.toString());
+            mseData.put("client_latlong", locationFormat);
 
             zone = mapHierachy.split(">");
 
-            mseData.put("sta_mac_address_campus", zone[0]);
-            mseData.put("sta_mac_address_building", zone[1]);
-            mseData.put("sta_mac_address_floor", zone[2]);
+            mseData.put("client_campus", zone[0]);
+            mseData.put("client_building", zone[1]);
+            mseData.put("client_floor", zone[2]);
 
             state = location.get("dot11Status").toString();
             if (state.equals("ASSOCIATED")) {
                 ArrayList ip = (ArrayList) location.get("ipAddress");
-                mseData.put("wlan_ssid", location.get("ssId").toString());
-                mseData.put("ap_mac", location.get("apMacAddress").toString());
+                mseData.put("wireless_id", location.get("ssId").toString());
+                mseData.put("wireless_station", location.get("apMacAddress").toString());
                 if (ip != null) {
                     mseDataDruid.put("src", ip.get(0).toString());
                 }
