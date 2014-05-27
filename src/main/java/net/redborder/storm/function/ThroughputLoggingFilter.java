61fe3b807f77f8611a473fe3c205d98655a72b56
Carlos Rodriguez <crresse@gmail.com>
2014-05-27 12:08:15 +0200

    Updated ThroughputLoggingFilter to show mean every minute

--- src/main/java/net/redborder/storm/function/ThroughputLoggingFilter.java
+++ src/main/java/net/redborder/storm/function/ThroughputLoggingFilter.java
@@ -7,7 +7,6 @@ package net.redborder.storm.function;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import storm.trident.operation.BaseFilter;
 import storm.trident.operation.TridentOperationContext;
 import storm.trident.tuple.TridentTuple;
@@ -15,10 +14,14 @@ import storm.trident.tuple.TridentTuple;
 public class ThroughputLoggingFilter extends BaseFilter {
 
     Map<Long, Integer> _count;
+    double _mean;
+    int _sec_count;
 
     @Override
     public void prepare(Map conf, TridentOperationContext context) {
         _count = new HashMap<>();
+        _sec_count = 0;
+        _mean = 0;
     }
 
     @Override
@@ -29,10 +32,22 @@ public class ThroughputLoggingFilter extends BaseFilter {
             Integer count = _count.get(timestamp);
             _count.put(timestamp, count + 1);
         } else {
-            if (_count.containsKey(timestamp - 1))
-                System.out.println(_count.get(timestamp - 1));
+            if (_count.containsKey(timestamp - 1)) {
+                int msgs = _count.get(timestamp - 1);
+                System.out.println(msgs);
+                
+                if (_sec_count <= 60) {
+                    _mean += msgs;
+                } else {
+                    _mean = _mean / 60;
+                    System.out.println("STATS THROUGHPUT - MEAN " + _mean);
+                    _sec_count = 0;
+                    _mean = 0;
+                }
+            }
             
             _count.put(timestamp, 1);
+            _sec_count += 1;
         }
         
         return true;
